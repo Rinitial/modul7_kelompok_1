0dc0f512ca376edbd2b78600ae9d644ebf8ba6e4
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-04-09 16:23:27 +0700

    menambahkan class DataMahasiswa

---
+++ DataMahasiswa_103022330076.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+
+public class DataMahasiswa_103022330076
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public int age { get; set; }
+    public Address address { get; set; }
+    public Courses courses { get; set; }
+
+    public class Address()
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Courses()
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public void ReadJSON()
+	{
+        string dataJSON = File.ReadAllText("jurnal7_1_103022330076.json");
+        var data = JsonConvert.DeserializedObject<DataMahasiswa_103022330076>(dataJSON);
+        Console.Write(data.firstName + " " + data.lastName + " berumur " + data.age + " tinggal di " + data.address.streetAddress + ", di kota" + data.address.city + ", di wilayah " + data.address.state + ", mengambil course: ");
+        for (int i = 0; i < data.courses.count)
+        {
+            Console.Write("Course code: " + data.courses.code + " dengan course bernama " + data.courses.name);
+        }
+	}
+}
+
+public class ProGRAM
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_103022330076 dataMHS = new DataMahasiswa_103022330076();
+        dataMHS.ReadJSON();
+    }
+}
---
+++ modul7_kelompok_1/DataMahasiswa_103022330076.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+
+public class DataMahasiswa_103022330076
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public int age { get; set; }
+    public Address address { get; set; }
+    public List<Courses> courses { get; set; }
+
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Courses
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public void ReadJSON()
+	{
+        string dataJSON = File.ReadAllText("jurnal7_1_103022330076.json");
+        var data = JsonConvert.DeserializeObject<DataMahasiswa_103022330076>(dataJSON);
+        Console.Write(data.firstName + " " + data.lastName + " berumur " + data.age + " tinggal di " + data.address.streetAddress + ", di kota" + data.address.city + ", di wilayah " + data.address.state + ", mengambil course: ");
+        Console.WriteLine(" ");
+        Console.WriteLine("========= COURSES =========");
+        for (int i = 0; i < data.courses.Count; i++)
+        {
+            Console.Write("Course code: " + data.courses[i].code + " dengan course bernama " + data.courses[i].name);
+            Console.WriteLine(" ");
+        }
+	}
+}
+
+public class ProGRAM
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_103022330076 dataMHS = new DataMahasiswa_103022330076();
+        dataMHS.ReadJSON();
+    }
+}
